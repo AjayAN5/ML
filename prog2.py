74bff5c53a87f8d024ebc49957de2e57e64ca8f3
AjayAN5 <47602748+AjayAN5@users.noreply.github.com>
2021-01-25 19:23:17 +0530

    Create prog2.py

---
+++ prog2.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+data=pd.read_csv('finds.csv')
+concepts = np.array(data.iloc[:, 0:-1])
+target = np.array(data.iloc[:, -1])
+
+
+def learn(concepts, target):
+    specific_h = concepts[0].copy()
+    general_h = [["?" for i in range(len(specific_h))]
+                 for i in range(len(specific_h))]
+
+    for i, h in enumerate(concepts):
+        if target[i] == "Yes":
+            for x in range(len(specific_h)):
+                if h[x] != specific_h[x]:
+                    specific_h[x] = '?'
+                    general_h[x][x] = '?'
+        if target[i] == "No":
+            for x in range(len(specific_h)):
+                if h[x] != specific_h[x]:
+                    general_h[x][x] = specific_h[x]
+                else:
+                    general_h[x][x] = '?'
+        
+        print("Iteration[" + str(i+1) + "]")
+        print("Specific: " + str(specific_h))
+        print("General: " + str(general_h) + "\n")
+        
+    general_h=[general_h[i] for i, h in enumerate(general_h) if h!= ['?' for x in range(len(specific_h))]]
+    return specific_h,general_h
+
+specific,general=learn(concepts,target)
+
+print("Final Hypothesis: ")
+print("Specific hypothesis: " + str(specific))
+print("General hypothesis: " + str(general))
