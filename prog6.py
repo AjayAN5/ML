53537d66602a2a0400d4c134c06c1a63a4074dfe
AjayAN5 <47602748+AjayAN5@users.noreply.github.com>
2021-01-26 19:57:53 +0530

    Update prog6.py

--- prog6.py
+++ prog6.py
@@ -6,15 +6,15 @@ import pandas as pd
 
 msg = pd.read_csv('naviebayes_text.csv', names=[
                   'message', 'label'])  # Tabular form data
-print('Total instances in the dataset:', msg.shape[0])
+#print('Total instances in the dataset:', msg.shape[0])
 msg['labelnum'] = msg.label.map({'pos': 1, 'neg': 0})
 
 X = msg.message
 Y = msg.labelnum
-print('\nThe message and its label of first 5 instances are listed below')
+'''print('\nThe message and its label of first 5 instances are listed below')
 X5, Y5 = X[0:6], msg.label[0:6]
 for x, y in zip(X5, Y5):
-    print(x, ',', y)
+    print(x, ',', y)'''
 
 # Splitting the dataset into train and test data
 xtrain, xtest, ytrain, ytest = train_test_split(X, Y)
@@ -27,10 +27,10 @@ print('Total testing instances :', xtest.shape[0])
 count_vect = CountVectorizer()
 xtrain_dtm = count_vect.fit_transform(xtrain)  # Sparse matrix
 xtest_dtm = count_vect.transform(xtest)
-print('\nTotal features extracted using CountVectorizer:', xtrain_dtm.shape[1])
-print('\nFeatures for first 5 training instances are listed below')
-df = pd.DataFrame(xtrain_dtm.toarray(), columns=count_vect.get_feature_names())
-print(df[0:5])  # tabular representation
+#print('\nTotal features extracted using CountVectorizer:', xtrain_dtm.shape[1])
+#print('\nFeatures for first 5 training instances are listed below')
+#df = pd.DataFrame(xtrain_dtm.toarray(), columns=count_vect.get_feature_names())
+#print(df[0:5])  # tabular representation
 
 # print(xtrain_dtm) #Same as above but sparse matrix representation
 # Training Naive Bayes (NB) classifier on training data.
